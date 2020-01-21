26a41335eb06128404da9d635a567304437a3b16
ComadoDragon <gokmensaral@gmail.com>
2020-01-21 22:07:31 +0300

    bind ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cart_with_provider/ui/product_list.dart';
+import 'package:flutter_cart_with_provider/viewmodel/api_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,12 +9,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Cart Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          builder: (context) => ApiViewModel(),
+        ),
+      ],
+      child: Consumer<ApiViewModel>(
+        builder: (context, ApiViewModel, _) {
+          return MaterialApp(
+            title: 'Flutter Cart Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: ProductListPage(title: 'Flutter Product List Page'),
+          );
+        },
       ),
-      home: ProductListPage(title: 'Flutter Product List Page'),
     );
   }
 }
---
+++ lib/viewmodel/api_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ApiViewModel with ChangeNotifier {
+
+
+
+}
